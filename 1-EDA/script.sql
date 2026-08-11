df610c887f0b500ff7c2724966f9f94e75b33cbf
Henrique Figueiredo <hft@cesar.school>
2026-08-11 17:58:35 -0300

    refatorando de acordo com o desafio

---
+++ 1-EDA/script.sql
@@ -0,0 +1,12 @@
+SELECT COUNT(*) AS total_colunas
+FROM information_schema.columns
+WHERE table_name = 'orders';
+
+SELECT 
+    COUNT(*) AS total_linhas,
+    MIN(created_at) AS data_minima,
+    MAX(created_at) AS data_maxima,
+    MIN(total) AS valor_minimo,
+    MAX(total) AS valor_maximo,
+    AVG(total) AS valor_medio
+FROM orders;
